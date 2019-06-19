8028ac7beecc370b206cbae4d1273b05d796c298
Mars Romulo <marsromulo@Marss-MacBook-Air.local>
2019-06-19 14:08:32 +0800

    first commit

---
+++ precache-manifest.0f1e058bf6ebc9007806f9f7d8208893.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae573408f78b69b9dcb3699c45b591a3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "81dc02112d2f8fd64f87",
+    "url": "./static/css/main.f9c67ae1.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "./static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "81dc02112d2f8fd64f87",
+    "url": "./static/js/main.643ebfc4.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
